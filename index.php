651db0aad94343e0da23b05313ddeb1cacbe7ed0
Navneet sharma <navneetnnavneet1@gmail.com>
2015-08-21 19:15:59 +0530

    added content

--- index.php
+++ index.php
@@ -29,23 +29,39 @@ if (!empty($_GET['location'])){
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title> Reliefinneed </title>
+    <title> Maps </title>
 
     <!-- Loading flat-ui-->
     <link rel="stylesheet" type="text/css" href="bower_components/flat-ui/dist/css/vendor/bootstrap.min.css">
   
     <!-- Loading flat-ui-->
     <link rel="stylesheet" type="text/css" href="bower_components/flat-ui/dist/css/flat-ui.min.css">
-    <link rel="stylesheet" type="text/css" href="bower_components/flat-ui/dist/css/timeline.css">
+
     <link rel="stylesheet" type="text/css" href="bower_components/flat-ui/font-awesome-4.4.0/css/font-awesome.min.css">
 
+
+	<link rel="stylesheet" href="bower_components/css/animate.css">
+	<link rel="stylesheet" href="bower_components/css/magnific-popup.css">
+	<link rel="stylesheet" href="bower_components/flexslider/flexslider.css">
+	<link rel="stylesheet" href="bower_components/css/form-elements.css">
+	<link rel="stylesheet" href="public/assets/css/style.css">
+	<link rel="stylesheet" href="bower_components/css/media-queries.css">
     <!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
     <script src="bower_components/flat-ui/dist/js/vendor/jquery.min.js"></script>
     
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="bower_components/flat-ui/dist/js/vendor/video.js"></script>
     <script src="bower_components/flat-ui/dist/js/flat-ui.min.js"></script>
-
+        <script src="bower_components/js/jquery-1.11.1.min.js"></script> 
+        <script src="bower_components/js/jquery.backstretch.min.js"></script>
+        <script src="bower_components/js/wow.min.js"></script>
+        <script src="bower_components/js/retina-1.1.0.min.js"></script>
+        <script src="bower_components/js/jquery.magnific-popup.min.js"></script>
+        <script src="bower_components/flexslider/jquery.flexslider-min.js"></script>
+        <script src="bower_components/js/jflickrfeed.min.js"></script>
+        <script src="bower_components/js/masonry.pkgd.min.js"></script>
+        <script src="bower_components/js/jquery.ui.map.min.js"></script>
+        <script src="bower_components/js/scripts.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 
   </head>
@@ -140,7 +156,7 @@ if (!empty($_GET['location'])){
        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
           <span class="sr-only">Toggle navigation</span>
        </button>
-       <a class="navbar-brand" href="#"><img src="#" alt="Brand"> reliefineed</a>
+       <a class="navbar-brand" href="#"><!--<img src="#" alt="Brand"> -->reliefineed</a>
     </div>
     <div class="collapse navbar-collapse" id="navbar-collapse-01">
       <ul class="nav navbar-nav navbar-right">
@@ -160,6 +176,23 @@ if (!empty($_GET['location'])){
     </div><!-- /.navbar-collapse -->
         </nav><!-- /navbar -->
 
+
+      <div class="slider-2-container">
+            <div class="container">
+                <div class="row">
+                                      <div class="col-sm-8 col-sm-offset-2 slider-2-text wow fadeInUp">
+                        <h1><br></h1>
+                        <h1><br></h1>
+                        <h1><br></h1>
+                        <h1><br></h1>
+                        <h1><br></h1>
+	            		<!-- <p>load more lines here.</p> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
 <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
 </fb:login-button>
 
@@ -174,6 +207,25 @@ if (!empty($_GET['location'])){
 </div>
 
     <br/>
+		<div class="work-container">
+	    	<div class="container">
+
+		      <div class="row">
+		                          <div class="col-sm-4">
+		                <div class="work wow fadeInDown">
+
+		                    <h4>How to use this app ?</h4>
+		                     <ul>
+		                     	<li>Just login</li>
+		                     	<li>verify your facebook account</li>
+		                     	<li>Type any foriegn city name in navbar search box.</li>
+		                     	<li>It's simple as that!</li>
+		                     </ul>
+
+		                </div>
+		            </div>
+		       <div class="col-sm-8">
+		         <div class="work wow fadeInUp">
     <?php
     if(!empty($breezometer_array)){
        $country = $breezometer_array['country_name'];
@@ -232,5 +284,14 @@ if (!empty($_GET['location'])){
       }
     }
     ?>
+    		                </div>
+	                </div>
+              </div>
+    
+
+	            </div>
+             </div>
+          </div>
+
   </body>
 </html>
